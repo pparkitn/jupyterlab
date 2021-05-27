6fd7b08311eecac5f577cfbfc91d22e4ce67c1ab
Piotr Parkitny <47726961+pparkitn@users.noreply.github.com>
2021-05-27 16:41:27 -0400

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -4,4 +4,3 @@ docker build -t myimage .
 docker tag myimage pparkitn/jupyterlab
 docker login
 docker push pparkitn/jupyterlab
-sudo docker run -p 8888:8888 -v $(pwd):/home pparkitn/jupyterlab jupyter-lab --ip 0.0.0.0 --allow-root --no-browser --NotebookApp.token=''
